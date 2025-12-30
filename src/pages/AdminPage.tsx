e56abfdf880a3428ee3e993b0a5572b96414c474
Nick Montross <n.montross1@gmail.com>
2025-12-30 11:30:37 -0500

    move location higher

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -790,6 +790,21 @@ export default function AdminPage() {
                   />
                 </div>
 
+                {/* Location */}
+                <div className="space-y-1">
+                  <label className="text-sm font-medium text-gray-700">
+                    Location
+                  </label>
+                  <input
+                    type="text"
+                    className="w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-1"
+                    value={newEvent.location}
+                    onChange={(e) =>
+                      setNewEvent((p) => ({ ...p, location: e.target.value }))
+                    }
+                  />
+                </div>
+
                 {/* Start Date */}
                 <div className="space-y-1">
                   <label
@@ -829,21 +844,6 @@ export default function AdminPage() {
                       setNewEvent((p) => ({ ...p, end_date: e.target.value }))
                     }
                     required
-                  />
-                </div>
-
-                {/* Location */}
-                <div className="space-y-1">
-                  <label className="text-sm font-medium text-gray-700">
-                    Location
-                  </label>
-                  <input
-                    type="text"
-                    className="w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-1"
-                    value={newEvent.location}
-                    onChange={(e) =>
-                      setNewEvent((p) => ({ ...p, location: e.target.value }))
-                    }
                   />
                 </div>
 
